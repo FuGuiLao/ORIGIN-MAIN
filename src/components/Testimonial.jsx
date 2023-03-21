29122101cc45106703262fd95b3170242ffed7c6
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-20 19:12:09 -0700

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -19,23 +19,7 @@ export function Testimonial({ id, author, children }) {
           <blockquote className="mt-10 font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-center">
             {children}
           </blockquote>
-          <figcaption className="mt-10 flex items-center sm:justify-center">
-            <div className="overflow-hidden rounded-full bg-slate-200">
-              <Image
-                className="h-12 w-12 object-cover"
-                src={author.image}
-                alt=""
-                width={48}
-                height={48}
-              />
-            </div>
-            <div className="ml-4">
-              <div className="text-base font-medium leading-6 tracking-tight text-slate-900">
-                {author.name}
-              </div>
-              <div className="mt-1 text-sm text-slate-600">{author.role}</div>
-            </div>
-          </figcaption>
+       
         </figure>
       </Container>
     </aside>
