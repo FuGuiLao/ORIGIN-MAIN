a520e23e3f3030895d0a9e6699c66fb351d83ba3
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-20 19:06:12 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,7 +14,6 @@ import { Testimonial } from '@/components/Testimonial'
 import { Testimonials } from '@/components/Testimonials'
 import avatarImage1 from '@/images/avatars/avatar-1.png'
 import avatarImage2 from '@/images/avatars/avatar-2.png'
-import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -45,11 +44,7 @@ export default function Home() {
         <p>
           Origin is a member of the California Superior Court Investigator Panel. This means that we can provide criminal case investigative services at no cost to qualified defendants. We also offer thses services in partnership with law firms through our fully integrated case review process. Our highlay experienced investigators can help make the difference.
         </p>
-        <div className="mt-8 flex gap-4">
-              <Button href="#free-chapters" color="blue">
-                Schedule a Consultation
-              </Button>
-            </div>
+       
       </Testimonial>
  
       <Screencasts />
@@ -65,11 +60,7 @@ export default function Home() {
         <p>
           Origin specialized in hard to get information, including bank and other asset searches, insurance policy information, employment data, medical records, travel information, and social media activity. Our specialized data services can help to uncover hidden information that can be valuable in civil proceedings. In addition to raw data, our experienced investigators and intelligence analysts can bring relevance to complex informaiton. 
         </p>
-        <div className="mt-8 flex gap-4">
-              <Button href="#free-chapters" color="blue">
-                Request Now
-              </Button>
-            </div>
+        
       </Testimonial>
       <Resources />
       <FreeChapters />
