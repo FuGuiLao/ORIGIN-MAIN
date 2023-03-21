ee1d30f087e88e7552ac8a8290e2391af4d54fa5
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-20 18:34:26 -0700

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -12,12 +12,13 @@ export function Testimonial({ id, author, children }) {
       className="relative bg-slate-100 py-16 sm:py-32"
     >
       <div className="text-slate-900/10">
-        <GridPattern x="50%" patternTransform="translate(0 80)" />TEST1
+        <GridPattern x="50%" patternTransform="translate(0 80)" />
       </div>
-      <Container size="xs" className="relative">TEST2
-        <figure>TEST3
+      <Container size="xs" className="relative">
+        <figure>
           <blockquote className="mt-10 font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-center">
             {children}
+            Origin is a member of the California Superior Court Investigator Panel. This means that we can provide criminal case investigative services at no cost to qualified defendants. We also offer thses services in partnership with law firms through our fully integrated case review process. Our highlay experienced investigators can help make the difference.
           </blockquote>
           <figcaption className="mt-10 flex items-center sm:justify-center">
             <div className="overflow-hidden rounded-full bg-slate-200">
