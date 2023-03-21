2b1a7093c310c416a1741f0461c5065df653b05e
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-20 18:52:38 -0700

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,1 +1,44 @@
+import Image from 'next/image'
 
+import { Container } from '@/components/Container'
+import { GridPattern } from '@/components/GridPattern'
+import { StarRating } from '@/components/StarRating'
+
+export function Testimonial({ id, author, children }) {
+  return (
+    <aside
+      id={id}
+      aria-label={`Testimonial from ${author.name}`}
+      className="relative bg-slate-100 py-16 sm:py-32"
+    >
+      <div className="text-slate-900/10">
+        <GridPattern x="50%" patternTransform="translate(0 80)" />
+      </div>
+      <Container size="xs" className="relative">
+        <figure>
+          <blockquote className="mt-10 font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-center">
+            {children}
+            Origin is a member of the California Superior Court Investigator Panel. This means that we can provide criminal case investigative services at no cost to qualified defendants. We also offer thses services in partnership with law firms through our fully integrated case review process. Our highlay experienced investigators can help make the difference.
+          </blockquote>
+          <figcaption className="mt-10 flex items-center sm:justify-center">
+            <div className="overflow-hidden rounded-full bg-slate-200">
+              <Image
+                className="h-12 w-12 object-cover"
+                src={author.image}
+                alt=""
+                width={48}
+                height={48}
+              />
+            </div>
+            <div className="ml-4">
+              <div className="text-base font-medium leading-6 tracking-tight text-slate-900">
+                {author.name}
+              </div>
+              <div className="mt-1 text-sm text-slate-600">{author.role}</div>
+            </div>
+          </figcaption>
+        </figure>
+      </Container>
+    </aside>
+  )
+}
