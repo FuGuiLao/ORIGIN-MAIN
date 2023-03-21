e055a16ffce7c5e6d5db78282335b46491590b56
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-20 19:06:53 -0700

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -15,7 +15,7 @@ export function Testimonial({ id, author, children }) {
       <div className="text-slate-900/10">
         <GridPattern x="50%" patternTransform="translate(0 80)" />
       </div>
-      <Container size="xs" className="relative">
+      <Container size="s" className="relative">
         <figure>
           <blockquote className="mt-10 font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-center">
             {children}
