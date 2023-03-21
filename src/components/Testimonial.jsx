b4cd5a7b7a75b0c84490fa1e6ea9ef7c6ac79795
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-21 11:00:38 -0700

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,6 +6,12 @@ import { StarRating } from '@/components/StarRating'
 
 export function Testimonial({ id, author, children }) {
   return (
-   
+    <aside
+      id={id}
+      aria-label={`Testimonial from ${author.name}`}
+      className="relative bg-zinc-100 py-16 sm:py-32"
+    >
+     
+    </aside>
   )
 }
