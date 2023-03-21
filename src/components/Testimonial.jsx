6318359e5251118c70066a71d1b79fbcb8befff8
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-21 11:00:05 -0700

    Update Testimonial.jsx

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,22 +6,6 @@ import { StarRating } from '@/components/StarRating'
 
 export function Testimonial({ id, author, children }) {
   return (
-    <aside
-      id={id}
-      aria-label={`Testimonial from ${author.name}`}
-      className="relative bg-zinc-100 py-16 sm:py-32"
-    >
-      <div className="text-slate-900/10">
-        <GridPattern x="50%" patternTransform="translate(0 80)" />
-      </div>
-      <Container size="xs" className="relative">
-        <figure>
-          <blockquote className="mt-10 font-display text-4xl font-medium tracking-tight text-slate-900 sm:text-center">
-            {children}
-          </blockquote>
-       
-        </figure>
-      </Container>
-    </aside>
+   
   )
 }
