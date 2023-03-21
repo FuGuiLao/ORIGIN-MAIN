5293bf7a088222bbbee06f54fc015fd9a0b02708
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-21 00:08:57 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ import coverImage from '@/images/Origin_Logo.png'
 function Testimonial() {
   return (
     <figure className="relative mx-auto max-w-md text-center lg:mx-0 lg:text-left">
-      <div className="flex justify-center text-blue-600 lg:justify-start">
+      <div className="flex justify-center text-red-800 lg:justify-start">
         <StarRating />
       </div>
       <blockquote className="mt-2">
@@ -17,7 +17,7 @@ function Testimonial() {
         </p>
       </blockquote>
       <figcaption className="mt-2 text-sm text-slate-500">
-        <strong className="font-semibold text-blue-600 before:content-['—_']">
+        <strong className="font-semibold text-red-800 before:content-['—_']">
           Partner
         </strong>
         , Los Angeles-Based Law Firm
@@ -28,7 +28,7 @@ function Testimonial() {
 
 export function Hero() {
   return (
-    <header className="overflow-hidden bg-slate-100 lg:bg-transparent lg:px-5">
+    <header className="overflow-hidden bg-gray-100 lg:bg-transparent lg:px-5">
       <div className="mx-auto grid max-w-6xl grid-cols-1 grid-rows-[auto_1fr] gap-y-16 pt-16 md:pt-20 lg:grid-cols-12 lg:gap-y-20 lg:px-3 lg:pb-36 lg:pt-20 xl:py-32">
         <div className="relative flex items-end lg:col-span-5 lg:row-span-2">
           <div className="absolute -top-20 -bottom-12 left-0 right-1/2 z-10 rounded-br-6xl bg-red-800 text-white/10 md:bottom-8 lg:-inset-y-32 lg:right-full lg:left-[-100vw] lg:-mr-40">
