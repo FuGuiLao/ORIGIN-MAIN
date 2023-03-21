8b5f21cdc6334bfd3193f337b0563010668a8812
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-20 21:51:31 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,7 +21,7 @@ export default function Home() {
     <>
       <Head>
         <title>
-          ORIGIN / Investigation / Intelligenc
+          ORIGIN / Investigation / Intelligence
         </title>
         <meta
           name="description"
