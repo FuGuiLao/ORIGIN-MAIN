363521c69c451fa566709d641bb319dfe9c050c5
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-21 09:14:23 -0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ import coverImage from '@/images/origin.png'
 function Testimonial() {
   return (
     <figure className="relative mx-auto max-w-md text-center lg:mx-0 lg:text-left">
-      <div className="flex justify-center text-red-800 lg:justify-start">
+      <div className="flex justify-center text-gray-800 lg:justify-start">
         <StarRating />
       </div>
       <blockquote className="mt-2">
