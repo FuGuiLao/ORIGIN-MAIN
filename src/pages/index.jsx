925d000e4640d237ac66fe8811f7b75f054d98c6
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-20 18:45:58 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,6 +11,7 @@ import { Resources } from '@/components/Resources'
 import { Screencasts } from '@/components/Screencasts'
 import { TableOfContents } from '@/components/TableOfContents'
 import { Testimonial } from '@/components/Testimonial'
+import { SuperiorCourt } from '@/components/SuperiorCourt'
 import { Testimonials } from '@/components/Testimonials'
 import avatarImage1 from '@/images/avatars/avatar-1.png'
 import avatarImage2 from '@/images/avatars/avatar-2.png'
@@ -33,7 +34,7 @@ export default function Home() {
       <NavBar />
       <TableOfContents />
       <Screencasts />
-    
+      <SuperiorCourt />
       <Resources />
       <FreeChapters />
       <Pricing />
