676db53bae8919e29b49330888ceaa770b4c3fc4
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-21 10:40:55 -0700

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -95,7 +95,7 @@ export function NavBar() {
                 <>
                   <span
                     aria-hidden="true"
-                    className="font-mono text-sm text-blue-600"
+                    className="font-mono text-sm text-red-600"
                   >
                     {(mobileActiveIndex + 1).toString().padStart(2, '0')}
                   </span>
@@ -130,7 +130,7 @@ export function NavBar() {
                 >
                   <span
                     aria-hidden="true"
-                    className="font-mono text-sm text-blue-600"
+                    className="font-mono text-sm text-red-600"
                   >
                     {(sectionIndex + 1).toString().padStart(2, '0')}
                   </span>
@@ -156,8 +156,8 @@ export function NavBar() {
                 className={clsx(
                   'flex w-full flex-col items-center justify-center border-b-2 before:mb-2 before:font-mono before:text-sm before:content-[counter(section,decimal-leading-zero)]',
                   sectionIndex === activeIndex
-                    ? 'border-blue-600 bg-blue-50 text-blue-600 before:text-blue-600'
-                    : 'border-transparent before:text-slate-500 hover:bg-blue-50/40 hover:before:text-slate-900'
+                    ? 'border-red-600 bg-red-50 text-red-600 before:text-red-600'
+                    : 'border-transparent before:text-slate-500 hover:bg-red-50/40 hover:before:text-slate-900'
                 )}
               >
                 {section.title}
