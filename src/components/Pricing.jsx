a12f3516a22e7b3ab51320924db5234ac6a20a86
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-23 12:29:11 -0700

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -40,7 +40,7 @@ function Plan({ name, description, price, features, href, featured }) {
           <span
             className={clsx(
               'text-[1.75rem] leading-tight',
-              featured ? 'text-white' : 'text-slate-500'
+              featured ? 'text-slate-900' : 'text-slate-500'
             )}
           >
             $
@@ -48,7 +48,7 @@ function Plan({ name, description, price, features, href, featured }) {
           <span
             className={clsx(
               'ml-1 mt-1 text-7xl tracking-tight',
-              featured ? 'text-slate-50' : 'text-slate-900'
+              featured ? 'text-white' : 'text-slate-900'
             )}
           >
             {price}
