67def751561c17af8b11063fcbc875421226e3ee
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-23 12:32:11 -0700

    Update SectionHeading.jsx

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -5,7 +5,7 @@ export function SectionHeading({ number, children, className, ...props }) {
     <h2
       className={clsx(
         className,
-        'inline-flex items-center rounded-full py-1 px-4 text-red-800 ring-1 ring-inset ring-blue-600'
+        'inline-flex items-center rounded-full py-1 px-4 text-red-800 ring-1 ring-inset ring-red-800'
       )}
       {...props}
     >
