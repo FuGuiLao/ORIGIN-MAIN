b7a9c2a941bc441e722f097eb72dd86531aeea3e
FuGuiLao <EAGLEMINY@GMAIL.COM>
2023-03-24 23:57:53 -0400

    UTF-8

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -16,7 +16,7 @@ export function Team() {
         <div className="mx-auto max-w-2xl sm:text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the
+            We're a dynamic group of individuals who are passionate about what we do and dedicated to delivering the
             best results for our clients.
           </p>
         </div>
