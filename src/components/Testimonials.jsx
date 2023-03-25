780eb39761640290229e261995b87867d22c3f8d
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-25 14:37:14 -0700

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -139,7 +139,7 @@ export function Testimonials() {
         <h2 className="font-display text-4xl font-bold tracking-tight text-zinc-900">
           Some kind words from our clients...
         </h2>
-     <div className="mx-auto mt-8 mb-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+     <div className="mx-auto mt-8 mb-8 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
             src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
