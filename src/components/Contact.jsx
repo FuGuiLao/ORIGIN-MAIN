827608cd9754af597720f7c45639c8ff01d96e20
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-25 16:17:13 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,12 +47,16 @@ export function Contact() {
           </div>
           <div>
             <address className="border-l border-red-800 pl-6 pt-0 not-italic text-zinc-50">
-            <p>LinkedIn</p>
-            <p>Facebook</p>
-            <p>Instagram</p>
-            <p>Twitter</p>
-            <p>TikTok</p>
-            </address>
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="currentColor"
+            viewBox="0 0 24 24">
+             <path
+            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+  </svg>
+
+  </address>
           </div>
         </div>
       </div>
