0dd68dd4dcfca5ebef48a2647f48ea44ec35e24c
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-25 16:09:32 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -53,17 +53,14 @@ export function Contact() {
               <p>TikTok</p>
               <p>Twitter</p>
 
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="h-7 w-7"
-    fill="currentColor"
-    style="color: #9146ff"
-    viewBox="0 0 24 24">
-    <path
-      d="M2.149 0l-1.612 4.119v16.836h5.731v3.045h3.224l3.045-3.045h4.657l6.269-6.269v-14.686h-21.314zm19.164 13.612l-3.582 3.582h-5.731l-3.045 3.045v-3.045h-4.836v-15.045h17.194v11.463zm-3.582-7.343v6.262h-2.149v-6.262h2.149zm-5.731 0v6.262h-2.149v-6.262h2.149z"
-      fill-rule="evenodd"
-      clip-rule="evenodd" />
-  </svg>
+<svg
+  class="w-6 h-6 text-blue-600 fill-current"
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24">
+  <path
+    d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
+  />
+</svg>
 
             </address>
           </div>
