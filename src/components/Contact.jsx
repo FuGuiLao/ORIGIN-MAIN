3fb6e49b45b5885b080f8d9db7e07aac83424a70
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-26 09:37:33 -0700

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,7 +47,22 @@ export function Contact() {
           </div>
           <div>
             <address className="border-l border-red-800 pl-6 pt-0 not-italic text-zinc-50 space-y-2">
-            <svg
+             <button
+    type="button"
+    data-te-ripple-init
+    data-te-ripple-color="light"
+    class="mb-2 inline-block rounded px-6 py-2.5 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+    style="background-color: #1877f2">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-4 w-4"
+      fill="currentColor"
+      viewBox="0 0 24 24">
+      <path
+        d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+    </svg>
+  </button>
+              <svg
     xmlns="http://www.w3.org/2000/svg"
     class="h-5 w-5"
     fill="currentColor"
