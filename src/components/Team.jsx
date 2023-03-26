2bb229f95771bb000dbad1104bf7aa3ec3249eb2
FuGuiLao <EAGLEMINY@GMAIL.COM>
2023-03-25 22:59:18 -0400

    expandable 1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,13 +5,13 @@ const people = [
   {
     name: 'John Smith',
     role: 'Investigator',
-    image: teamImage1,
+    image: 'teamImage1',
     bio: 'Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda. Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab. Magni labore molestiae nulla qui.',
   },
   {
     name: 'Steve Jones',
     role: 'Investigator',
-    image: teamImage2,
+    image: 'teamImage2',
     bio: 'Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda. Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab. Magni labore molestiae nulla qui.',
   },
   // More people...
@@ -26,6 +26,14 @@ export function Team() {
           role="list"
           className="mx-auto mt-0 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none"
         >
+         </Container>
+      <Expandable>
+        {({ isExpanded }) => (
+          <>
+            <ul
+              role="list"
+              className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 px-4 lg:max-w-7xl lg:grid-cols-3 lg:px-8"
+            >
           {people.map((person) => (
             <li key={person.name} className="flex flex-col gap-6 xl:flex-row">
               <img className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover" src={person.imageUrl} alt="" />
@@ -37,6 +45,11 @@ export function Team() {
             </li>
           ))}
         </ul>
+            <Expandable.Button>Read more testimonials</Expandable.Button>
+          </>
+        )}
+      </Expandable>
+    </section>
       </div>
     </div>
   )
