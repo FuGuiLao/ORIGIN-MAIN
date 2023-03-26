49d81eae0cd29dbbdf66c4785c27c533405ccf4f
FuGuiLao <EAGLEMINY@GMAIL.COM>
2023-03-26 01:46:04 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -28,33 +28,32 @@ export function Team() {
         {({ isExpanded }) => (
           <>
           <ol role="list" className="mt-16 space-y-10 sm:space-y-16">
-                {Object.entries(people)
-                  .slice(0, isExpanded ? undefined : 0)
-                  .map(([title, pages]) => (
-                    <li key={title}>
-                      <h3 className="font-display text-3xl font-bold tracking-tight text-slate-900">
-                        {title}
-                      </h3>
+            {Object.entries(people)
+              .slice(0, isExpanded ? undefined : 0)
+               .map(([title, pages]) => (
+                 <li key={title}>
+                   <h3 className="font-display text-3xl font-bold tracking-tight text-slate-900">
+                    {title}
+                    </h3>
 
-        <ul
-          role="list"
-          className="mx-auto mt-0 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none">
-          {people.map((person) => (
-            <li key={person.name} className="flex flex-col gap-6 xl:flex-row">
-              <img className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover" src={person.image} alt="" />
-              <div className="flex-auto">
-                <h3 className="text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
-                <p className="text-base leading-7 text-gray-600">{person.role}</p>
-                <p className="mt-6 text-base leading-7 text-gray-600">{person.bio}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-        </li>
-         <Expandable.Button>Read more testimonials</Expandable.Button>
-          </>
-        )}
-      </Expandable>
+                     <ul
+                       role="list"
+                         className="mx-auto mt-0 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none">
+                           {people.map((person) => (
+                             <li key={person.name} className="flex flex-col gap-6 xl:flex-row">
+                               <img className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover" src={person.image} alt="" />
+                                <div className="flex-auto">
+                                   <h3 className="text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
+                                   <p className="text-base leading-7 text-gray-600">{person.role}</p>
+                                   <p className="mt-6 text-base leading-7 text-gray-600">{person.bio}</p>
+                                </div>
+                              </li>
+                             ))}
+                      </ul>
+                 </li>
+        <Expandable.Button>Read more testimonials</Expandable.Button>
+        </>
+        </Expandable>
 
       </div>
     </div>
