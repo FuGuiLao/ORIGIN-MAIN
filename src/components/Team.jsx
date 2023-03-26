6a8e1efc9a67bd30f82913c3559052033a15a6d4
FuGuiLao <EAGLEMINY@GMAIL.COM>
2023-03-25 23:37:17 -0400

    expandable

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -22,6 +22,10 @@ export function Team() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
        
+      <Expandable>
+        {({ isExpanded }) => (
+          <>
+
         <ul
           role="list"
           className="mx-auto mt-0 grid max-w-2xl grid-cols-1 gap-x-6 gap-y-20 sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none"
@@ -29,7 +33,7 @@ export function Team() {
 
           {people.map((person) => (
             <li key={person.name} className="flex flex-col gap-6 xl:flex-row">
-              <img className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover" src={person.imageUrl} alt="" />
+              <img className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover" src={person.image} alt="" />
               <div className="flex-auto">
                 <h3 className="text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
                 <p className="text-base leading-7 text-gray-600">{person.role}</p>
@@ -38,6 +42,12 @@ export function Team() {
             </li>
           ))}
         </ul>
+
+         <Expandable.Button>Read more testimonials</Expandable.Button>
+          </>
+        )}
+      </Expandable>
+
       </div>
     </div>
   )
