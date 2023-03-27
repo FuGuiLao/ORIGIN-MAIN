37f977e8705c7cdcbfcb05531da5a0dbee36e8a0
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-26 17:27:26 -0700

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -142,35 +142,35 @@ export function Testimonials() {
      <div className="mx-auto mt-8 mb-8 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
+            src="http://www.origininvestigations.com/cdn/seal-casc.png"
             alt="Transistor"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
+            src="http://www.origininvestigations.com/cdn/seal-doj.png"
             alt="Reform"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
+            src="http://www.origininvestigations.com/cdn/seal-ic.png"
             alt="Tuple"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
+            src="http://www.origininvestigations.com/cdn/seal-state.png"
             alt="SavvyCal"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
+            src="http://www.origininvestigations.com/cdn/seal-bh.png"
             alt="Statamic"
             width={158}
             height={48}
