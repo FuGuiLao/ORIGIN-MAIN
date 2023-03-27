f59fb5cd03e3263e36fab75016056921c9c45ea6
FuGuiLao <EAGLEMINY@GMAIL.COM>
2023-03-27 01:59:05 -0400

    localize seals

--- src/components/LogoCloud.jsx
+++ src/components/LogoCloud.jsx
@@ -1,3 +1,12 @@
+import Image from 'next/image'
+
+import seal-bh from @/images/seal-bh.png
+import seal-casc from @/images/seal-casc.png
+import seal-doj from @/images/seal-doj.png
+import seal-ic from @/images/seal-ic.png
+import seal-la from @/images/seal-la.png
+import seal-state from @/images/seal-state.png
+
 export function LogoCloud() {
   return (
     <div className="bg-white py-24 sm:py-32">
@@ -8,36 +17,43 @@ export function LogoCloud() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
-            alt="Transistor"
+            src={seal-bh}
+            alt=""
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
-            alt="Reform"
+            src={seal-casc}
+            alt=""
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
-            alt="Tuple"
+            src={seal-doj}
+            alt=""
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
-            alt="SavvyCal"
+            src={seal-ic}
+            alt=""
             width={158}
             height={48}
           />
           <img
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
-            alt="Statamic"
+            src={seal-la}
+            alt=""
+            width={158}
+            height={48}
+          />
+          <img
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+            src={seal-la}
+            alt=""
             width={158}
             height={48}
           />
