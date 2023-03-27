52d4423ed95edccabebbc57d1ca052535ef5f3b0
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2023-03-26 18:09:23 -0700

    Update OurPeople.jsx

--- src/components/OurPeople.jsx
+++ src/components/OurPeople.jsx
@@ -20,7 +20,7 @@ export function OurPeople() {
       aria-labelledby="author-title"
       className="relative scroll-mt-14 pb-3 pt-8 sm:scroll-mt-32 sm:pb-16 sm:pt-10 lg:pt-16"
     >
-      <div className="absolute inset-x-0 bottom-0 top-1/2 text-slate-900/10 [mask-image:linear-gradient(transparent,white)]">
+      <div className="absolute inset-x-0 bottom-0 top-1/2 text-zinc-900/10 [mask-image:linear-gradient(transparent,white)]">
         <GridPattern x="50%" y="100%" />
       </div>
       <div className="relative mx-auto max-w-5xl pt-16 sm:px-6">
@@ -37,28 +37,28 @@ export function OurPeople() {
             <SectionHeading number="5" id="author-title">
               Our People
             </SectionHeading>
-            <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-slate-900 sm:text-6xl">
+            <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-zinc-900 sm:text-6xl">
               <span className="block text-red-800">Jayden Brant –</span> <bold>Hi, I am the founder and managing partner of Origin Investigations Inc.</bold>
             </p>
-            <p className="mt-4 text-lg tracking-tight text-slate-700">
+            <p className="mt-4 text-lg tracking-tight text-zinc-700">
 Throughout his career, Jayden has worked for the United States federal government in law enforcement and intelligence capacities. Jayden previously served as a Deputy Sheriff with the Los Angeles County Sheriff's Department. While there, he worked in field operations, custody, training bureau, and numerous special assignments within the department.
 </p>
-<p className="mt-4 text-lg tracking-tight text-slate-700">
+<p className="mt-4 text-lg tracking-tight text-zinc-700">
 He has extensive knowledge, training, and experience in criminal investigation, interrogation, undercover surveillance, case management, evidence handling, and California and Federal law.
 </p>
-<p className="mt-4 text-lg tracking-tight text-slate-700">
+<p className="mt-4 text-lg tracking-tight text-zinc-700">
 Jayden attained California Peace Officer Standards and Training (POST) Certification and completed POST Advanced Officer Training while with the LASD and continues to maintain certification. He also completed the challenging coursework for the POST Specialized Investigator Certification.
 </p>
-<p className="mt-4 text-lg tracking-tight text-slate-700">
+<p className="mt-4 text-lg tracking-tight text-zinc-700">
 During his career, Jayden has worked with the LASD, LAPD, US Marshals Service, Diplomatic Security Service, US Secret Service, Federal Bureau of Investigation, Department of Homeland Security, US Immigration and Customs Enforcement, the Superior Court of California, as well as numerous local law enforcement agencies and independent investigators.
 </p>
-<p className="mt-4 text-lg tracking-tight text-slate-700">
+<p className="mt-4 text-lg tracking-tight text-zinc-700">
 In addition to a law enforcement background, Jayden also has significant private sector experience. He has held leadership positions in the heavily regulated financial services industry not only overseeing operations of a Registered Investment Advisory Firm, but also conducting corporate due diligence and fraud investigations.
 </p>
-<p className="mt-4 text-lg tracking-tight text-slate-700">
+<p className="mt-4 text-lg tracking-tight text-zinc-700">
 He has also provided regulatory compliance, security, and legal consulting for high-profile individuals and numerous companies across a wide variety of industries. Jayden attended Claremont McKenna College and studied computer engineering.
 </p>
-<p className="mt-4 text-lg tracking-tight text-slate-700">
+<p className="mt-4 text-lg tracking-tight text-zinc-700">
 Jayden is a member of the California Association of Licensed Investigators, the United States Association of Professional Investigators, the Association of Former Intelligence Officers, and the International Association of Law Enforcement Intelligence Analysts.
 
 
