631a456498355dc452b76b23623ac21ce472f010
FuGuiLao <EAGLEMINY@GMAIL.COM>
2023-03-27 16:49:28 -0400

    1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -32,8 +32,8 @@ export function Banner() {
           <span className="sr-only">Close</span>
           <XMarkIcon  className="h-5 w-5 text-white" aria-hidden="true" />
         </button>
-        </Transition>
       </div>
+      </Transition>
     </div>
   )
 }
