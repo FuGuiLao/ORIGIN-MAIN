65957744e6181b98b86b8a2f6739daee370d854a
FuGuiLao <EAGLEMINY@GMAIL.COM>
2023-03-27 16:10:38 -0400

    1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,10 @@
 import { XMarkIcon  } from '@heroicons/react/20/solid'
+import { useState } from 'react'
 
 export function Banner() {
+
+    const [show, setShow] = useState(true);
+
   return (
     <div className="flex items-center gap-x-6 bg-gray-900 py-2.5 px-6 sm:px-3.5 sm:before:flex-1">
       <p className="text-sm leading-6 text-white">
@@ -13,7 +17,10 @@ export function Banner() {
         </a>
       </p>
       <div className="flex flex-1 justify-end">
-        <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+        <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+         onClick={() => {setShow(false)
+            }}
+            >
           <span className="sr-only">Dismiss</span>
           <XMarkIcon  className="h-5 w-5 text-white" aria-hidden="true" />
         </button>
