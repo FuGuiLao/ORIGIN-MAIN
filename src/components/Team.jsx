afcaa83ad1ccc996e03fd74f0a30987e12ed70c7
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-03-30 01:19:51 -0700

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -32,7 +32,7 @@ const people = [
 export function Team() {
   return (
     <section className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    
        <container>
     </container>
       <Expandable>
@@ -60,7 +60,7 @@ export function Team() {
         </>
         )}
       </Expandable>
-      </div>
+     
     </section>
   )
 }
