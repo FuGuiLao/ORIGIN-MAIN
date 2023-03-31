154991e6b4ab3b104d692fb771271e6f1d762d0c
eagleminy <EAGLEMINY@GMAIL.COM>
2023-03-31 01:11:42 -0400

    improt button

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/Container'
 import { SectionHeading } from '@/components/SectionHeading'
+import { Button } from '@/components/Button'
 
 
 export function SlideShow() { 
@@ -42,7 +43,7 @@ export function SlideShow() {
                                 alt="" />
                         </div>
                     </div>
-                    <button
+                    <Button
                         class="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
                         type="button"
                         data-te-target="#carouselExampleControls"
@@ -65,8 +66,8 @@ export function SlideShow() {
                             class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
                         >Previous</span
                         >
-                    </button>
-                    <button
+                    </Button>
+                    <Button
                         class="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
                         type="button"
                         data-te-target="#carouselExampleControls"
@@ -89,7 +90,7 @@ export function SlideShow() {
                             class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
                         >Next</span
                         >
-                    </button>
+                    </Button>
      </Container>
    </section>
   )
