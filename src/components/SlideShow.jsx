d222bfb3110bfe2354b31b35723893156378f1cb
eagleminy <EAGLEMINY@GMAIL.COM>
2023-04-03 19:02:19 -0400

    1

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,7 +1,7 @@
-﻿import React, { useState, useEffect } from 'react';
+﻿import React, { useState } from 'react';
 import { ArrowLeftCircleIcon, ArrowRightCircleIcon } from '@heroicons/react/24/solid';
 
-export function SlideShow() {
+export default function Slideshow() {
     const [slides, setSlides] = useState([
         'First slide',
         'Second slide',
@@ -9,42 +9,37 @@ export function SlideShow() {
     ]);
     const [currentSlide, setCurrentSlide] = useState(0);
 
-    useEffect(() => {
-        const slideInterval = setInterval(() => {
-            setCurrentSlide((currentSlide + 1) % slides.length);
-        }, 2000);
-
-        return () => {
-            clearInterval(slideInterval);
-        };
-    }, [currentSlide, slides.length]);
-
-    function handlePrevClick() {
+    const handlePrevClick = () =>
         setCurrentSlide((currentSlide - 1 + slides.length) % slides.length);
-    }
 
-    function handleNextClick() {
+    const handleNextClick = () =>
         setCurrentSlide((currentSlide + 1) % slides.length);
-    }
 
     return (
-        <div id="slideshow">
+        <div className="relative h-screen">
             <div className="absolute inset-y-0 left-0 flex items-center">
-                <button className="p-4" onClick={handlePrevClick}>
-                    <ArrowLeftCircleIcon className="h-6 w-6 text-white" />
+                <button
+                    className="p-4 text-white hover:text-gray-400 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150"
+                    onClick={handlePrevClick}
+                >
+                    <ArrowLeftCircleIcon className="h-6 w-6" />
                 </button>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center">
-                <button className="p-4" onClick={handleNextClick}>
-                    <ArrowRightCircleIcon className="h-6 w-6 text-white" />
+                <button
+                    className="p-4 text-white hover:text-gray-400 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150"
+                    onClick={handleNextClick}
+                >
+                    <ArrowRightCircleIcon className="h-6 w-6" />
                 </button>
             </div>
-            <div className="relative">
+            <div className="relative h-full">
                 {slides.map((slide, index) => (
                     <p
                         key={index}
-                        className={`absolute top-0 left-0 w-full h-full ${index === currentSlide ? '' : 'opacity-0'
+                        className={`absolute top-0 left-0 w-full h-full flex items-center justify-center text-white transition-opacity duration-500 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
                             }`}
+                        style={{ backgroundColor: '#333' }}
                     >
                         {slide}
                     </p>
