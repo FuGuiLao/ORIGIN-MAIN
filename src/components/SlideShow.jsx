f255927d49a668d0c623269d65110c03efba2b48
eagleminy <EAGLEMINY@GMAIL.COM>
2023-04-03 15:36:28 -0400

    1

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -1,7 +1,7 @@
 ﻿import React, { useState, useEffect } from 'react';
-import '@/styles/slideshow.css';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 
-function Slideshow() {
+function SlideShow() {
     const [slides, setSlides] = useState([
         'First slide',
         'Second slide',
@@ -28,20 +28,43 @@ function Slideshow() {
     }
 
     return (
-        <div id="slideshow">
-            <p className="prev" onClick={handlePrevClick}>
-                &lt;
-            </p>
-            {slides.map((slide, index) => (
-                <div key={index} className="slide-container">
-                    <p className={index === currentSlide ? 'active' : ''}>{slide}</p>
-                </div>
-            ))}
-            <p className="next" onClick={handleNextClick}>
-                &gt;
-            </p>
+        <div className="relative bg-gray-50">
+            <div className="relative w-full overflow-hidden">
+                {slides.map((slide, index) => (
+                    <div
+                        key={index}
+                        className={`${index === currentSlide ? 'z-10' : 'opacity-0'
+                            } absolute w-full h-full transition-opacity duration-500`}
+                    >
+                        <div className="absolute inset-0 bg-gray-500"></div>
+                        <div className="absolute inset-y-0 right-0 flex items-center pr-4">
+                            <p
+                                className="text-4xl font-bold text-white cursor-pointer"
+                                onClick={handleNextClick}
+                            >
+                                <ChevronRightIcon className="h-10 w-10" />
+                            </p>
+                        </div>
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-4">
+                            <p
+                                className="text-4xl font-bold text-white cursor-pointer"
+                                onClick={handlePrevClick}
+                            >
+                                <ChevronLeftIcon className="h-10 w-10" />
+                            </p>
+                        </div>
+                        <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32">
+                            <div className="w-full font-mono flex flex-col items-center relative">
+                                <p className="text-6xl text-white leading-none mb-2">
+                                    {slide}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
 
-export default Slideshow;
+export default SlideShow;
