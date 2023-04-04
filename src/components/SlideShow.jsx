3912a88ed8a7710816c2f94d2c78c98b82678cd8
eagleminy <EAGLEMINY@GMAIL.COM>
2023-04-03 22:25:40 -0400

    1

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -121,50 +121,27 @@ export function SlideShow() {
     setCurrentSlide((currentSlide + 1) % slides.length);
 
     return (
-        <div className="relative h-screen">
+        <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center">
-                <button
-                    className="p-4 text-white hover:text-gray-400 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150"
-                    onClick={handlePrevClick}
-                >
-                    <ArrowLeftCircleIcon className="h-6 w-6" />
+                <button className="p-4" onClick={handlePrevClick}>
+                    <ArrowLeftCircleIcon className="h-6 w-6 text-white" />
                 </button>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center">
-                <button
-                    className="p-4 text-white hover:text-gray-400 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150"
-                    onClick={handleNextClick}
-                >
-                    <ArrowRightCircleIcon className="h-6 w-6" />
+                <button className="p-4" onClick={handleNextClick}>
+                    <ArrowRightCircleIcon className="h-6 w-6 text-white" />
                 </button>
             </div>
-            <div className="relative h-full">
+            <div className="relative min-h-auto">
                 {slides.map((slide, index) => (
                     <p
                         key={index}
                         className={`absolute top-0 left-0 w-full h-full flex items-center justify-center text-white transition-opacity duration-500 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
                             }`}
-                        style={{ backgroundColor: '#333' }}
                     >
                         {slide}
                     </p>
                 ))}
-            </div>
-            <div className="absolute inset-x-0 bottom-0 flex justify-center pb-8">
-                <div className="flex space-x-4">
-                    <button
-                        className="p-2 text-white hover:text-gray-400 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150"
-                        onClick={handlePrevClick}
-                    >
-                        <ArrowLeftCircleIcon className="h-6 w-6" />
-                    </button>
-                    <button
-                        className="p-2 text-white hover:text-gray-400 focus:outline-none focus:text-gray-400 transition ease-in-out duration-150"
-                        onClick={handleNextClick}
-                    >
-                        <ArrowRightCircleIcon className="h-6 w-6" />
-                    </button>
-                </div>
             </div>
         </div>
     );
