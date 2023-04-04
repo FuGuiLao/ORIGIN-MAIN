0f863d6832e9ecf75e37398fbe9fcf3866c97308
eagleminy <EAGLEMINY@GMAIL.COM>
2023-04-03 20:58:04 -0400

    130,145 absolute>>relative

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -127,13 +127,13 @@ export function SlideShow() {
             className="relative scroll-mt-14 pb-3 pt-8 sm:scroll-mt-32 sm:pb-16 sm:pt-10 lg:pt-16"
         >
 
-      <div className="absolute inset-x-0 bottom-0 top-1/2 text-zinc-900/10 [mask-image:linear-gradient(transparent,white)]">
+            <div className="relative inset-x-0 bottom-0 top-1/2 text-zinc-900/10 [mask-image:linear-gradient(transparent,white)]">
                 <GridPattern x="50%" y="100%" />
             </div>
         {slides.map((slide, index) => (
           <p
             key={index}
-            className={`absolute top-0 left-0 w-full h-full flex items-center justify-center text-white transition-opacity duration-500 ${
+                className={`relative top-0 left-0 w-full h-full flex items-center justify-center text-white transition-opacity duration-500 ${
               index === currentSlide ? 'opacity-100' : 'opacity-0'
             }`}
             style={{ backgroundColor: '#333' }}
