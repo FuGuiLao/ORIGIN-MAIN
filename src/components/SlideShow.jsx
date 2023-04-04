5bed4dc1d5a1aae874cd6c033fb2a99b408f4f81
eagleminy <EAGLEMINY@GMAIL.COM>
2023-04-03 22:49:06 -0400

    2

--- src/components/SlideShow.jsx
+++ src/components/SlideShow.jsx
@@ -121,6 +121,7 @@ export function SlideShow() {
     setCurrentSlide((currentSlide + 1) % slides.length);
 
     return (
+
         <div className="relative h-screen">
             <div className="absolute inset-y-0 left-0 flex items-center">
                 <button className="p-4" onClick={handlePrevClick}>
@@ -136,12 +137,13 @@ export function SlideShow() {
                 {slides.map((slide, index) => (
                     <p
                         key={index}
-                        className={`relative top-0 left-0 w-full h-full flex items-center justify-center text-white transition-opacity duration-500 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
+                        className={`relative scroll-mt-14 pb-3 pt-8 sm:scroll-mt-32 sm:pb-16 sm:pt-10 lg:pt-16 ${index === currentSlide ? 'opacity-100' : 'opacity-0'
                             }`}
                         style={{ backgroundColor: '#333' }}
                     >
                         {slide}
                     </p>
+
                 ))}
             </div>
         </div>
