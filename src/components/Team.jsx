1830466ee05cb0db5c01e65516005198e7056dfb
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-04-07 13:55:19 -0700

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -45,6 +45,16 @@ export function Team() {
                 <section id="team" aria-labelledby="author-title" className="relative scroll-mt-14 pb-3 pt-8 sm:scroll-mt-32 sm:pb-16 sm:pt-10 lg:pt-16">
 
 
+<div className="bg-white py-24 sm:py-32">
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl sm:text-center">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
+                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                            We're a dynamic group of individuals who are passionate about what we do and dedicated to delivering the
+                            best results for our clients.
+                        </p>
+                    </div>
+
             
                     <ul
                         role="list"
