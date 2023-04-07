4fffe401f4fa83e9835c5dc3c5e4965a791dc028
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-04-07 14:29:45 -0700

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -55,11 +55,11 @@ export function Team() {
                     >
                         {people.map((person) => (
                             <li key={person.name} className="flex flex-col gap-6 xl:flex-row">
-                               
+                                <Image className="aspect-[4/5] w-52 flex-none rounded-2xl object-cover" src={person.image} alt="" />
                                 <div className="flex-auto">
-                                    <h3 className="text-lg font-semibold leading-8 tracking-tight text-gray-900">{person.name}</h3>
-                                    <p className="text-base leading-7 text-gray-600">{person.role}</p>
-                                    <p className="mt-6 text-base leading-7 text-gray-600">{person.bio}</p>
+                                    <h3 className="text-lg font-semibold leading-8 tracking-tight text-zinc-900">{person.name}</h3>
+                                    <p className="text-base leading-7 text-zinc-600">{person.role}</p>
+                                    <p className="mt-6 text-base leading-7 text-zinc-600">{person.bio}</p>
                                 </div>
                             </li>
                         ))}
