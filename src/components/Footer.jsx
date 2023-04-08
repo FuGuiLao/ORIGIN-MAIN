4d4aeaa7f5ee1e1e436bb68fb691380ab1f1ac0a
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-04-08 13:16:24 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,6 @@ export function Footer() {
         <p><a href="#terms-of-use">Terms of Use</a> | <a href="#privacy-policy">Privacy Policy</a> | <a href="#data-security">Data Security</a>
         </p>
       </div>
-    </footer>
-  )
-}
-
 <!-- Modal toggle -->
 <button data-modal-target="defaultModal" data-modal-toggle="defaultModal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
   Toggle modal
@@ -52,3 +48,7 @@ export function Footer() {
         </div>
     </div>
 </div>
+ 
+</footer>
+  )
+}
