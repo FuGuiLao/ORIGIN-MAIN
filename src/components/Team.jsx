f25efb7eea7857dfc3fd7cf72d1b071ce5d06b89
eagleminy <EAGLEMINY@GMAIL.COM>
2023-04-10 19:28:20 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -23,46 +23,8 @@ const people = [
     image: teamImage2,
     bio: 'Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda. Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab. Magni labore molestiae nulla qui.',
 
-    modal:    function Modal() {
-
-            return(
-         <>
-            <button
-                type="button"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={() => setOpen(true)}
-            >
-                Open Modal
-            </button>
-
-        <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" onClose={setOpen}>
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                </Transition.Child>
-
-                <div className="fixed inset-0 z-10 overflow-y-auto">
-                    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
-
-                                <div className="relative mx-auto max-w-5xl pt-16 sm:px-6">
+    modal:
+                                
                                     <div className="bg-zinc-50 pt-px sm:rounded-6xl">
                                         <div className="relative mx-auto -mt-16 h-44 w-44 overflow-hidden rounded-full bg-zinc-300 md:float-right md:h-64 md:w-64 md:[shape-outside:circle(40%)] lg:mr-20 lg:h-72 lg:w-72">
                                             <Image
@@ -112,22 +74,10 @@ const people = [
                                             </p>
                                         </div>
                                     </div>
-                                </div>
+                              
 
-                            </Dialog.Panel>
-                        </Transition.Child>
-                    </div>
-                </div>
-            </Dialog>
-            </Transition.Root>
-        </>
-    )
-}
+  },
 
-
-
-
-    },
   {
     name: 'Victoria Vannoy',
     role: 'Director / Intelligence',
@@ -171,6 +121,8 @@ export function Team() {
         setIsExpanded(!isExpanded)
     }
 
+    const [isOpen, setIsOpen] = useState(false)
+
 
     return (
         <Expandable>
@@ -193,7 +145,51 @@ export function Team() {
                                     <h3 className="text-lg font-semibold leading-8 tracking-tight text-zinc-900">{person.name}</h3>
                                     <p className="text-base leading-7 text-zinc-600">{person.role}</p>
                                     <p className="mt-6 text-base leading-7 text-zinc-600">{person.bio}</p>
-                                    <p className="mt-6 text-base leading-7 text-zinc-600">{person.modal}</p>
+
+                                       <>
+                                           <button className="text-blue-500" onClick={() => setIsOpen(true)}>
+                                               Click here
+                                           </button>
+                                           <Transition.Root show={open} as={Fragment}>
+                                               <Dialog as="div" className="relative z-10" onClose={setOpen}>
+                                                   <Transition.Child
+                                                       as={Fragment}
+                                                       enter="ease-out duration-300"
+                                                       enterFrom="opacity-0"
+                                                       enterTo="opacity-100"
+                                                       leave="ease-in duration-200"
+                                                       leaveFrom="opacity-100"
+                                                       leaveTo="opacity-0"
+                                                   >
+                                                       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                                                   </Transition.Child>
+
+                                                   <div className="fixed inset-0 z-10 overflow-y-auto">
+                                                       <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                                           <Transition.Child
+                                                               as={Fragment}
+                                                               enter="ease-out duration-300"
+                                                               enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                                               enterTo="opacity-100 translate-y-0 sm:scale-100"
+                                                               leave="ease-in duration-200"
+                                                               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                                                               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                                                           >
+                                                               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+
+                                                                   <div className="relative mx-auto max-w-5xl pt-16 sm:px-6">
+                                                                       {person.modal}
+                                                                   </div>
+                                                               </Dialog.Panel>
+                                                           </Transition.Child>
+                                                       </div>
+                                                   </div>
+                                               </Dialog>
+                                           </Transition.Root>
+                                       </>
+                                       )
+
+
                                 </div>
                             </li>
                            ))}
