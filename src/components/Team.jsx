80d36c52661e509ccb5f9dd44729c5e79976f09d
Yi Min <eagleminy@gmail.com>
2023-04-20 18:43:12 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -28,9 +28,11 @@ const people = [
 
     modal:
 
+        <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
 
 
-                <><div>
+
+            <div>
 
             <div className="mt-3 text-center sm:mt-5">
                 <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
@@ -78,7 +80,8 @@ const people = [
 
                 </div>
             </div>
-        </div><div className="mt-5 sm:mt-6">
+        </div>
+            <div className="mt-5 sm:mt-6">
                 <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -86,8 +89,10 @@ const people = [
                 >
                     Go back to dashboard
                 </button>
-            </div></>
+            </div>
 
+
+        </Dialog.Panel>
 
 
 
@@ -190,7 +195,7 @@ export function Team() {
                                                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                                                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                                            >
-                                                               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                                                               
 
                                                                    
                                                                        
@@ -200,7 +205,7 @@ export function Team() {
                                                                        
                                                                    
 
-                                                               </Dialog.Panel>
+                                                               
                                                            </Transition.Child>
                                                        </div>
                                                    </div>
