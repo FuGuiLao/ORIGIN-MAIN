f7879ac0b20a39a7f56e7507f2c9c21fb6250d7c
Yi Min <eagleminy@gmail.com>
2023-04-20 20:41:28 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -34,8 +34,7 @@ const people = [
 
 
         <>
-            <div className="my-4 sm:my-6 md:my-8 lg:my-10">
-            <div className="mt-3 text-center sm:mt-5">
+
             <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
                 Jayden Brant
             </Dialog.Title>
@@ -80,7 +79,8 @@ const people = [
 
 
             </div>
-        </div><div className="mt-5 sm:mt-6">
+
+        <div className="mt-5 sm:mt-6">
                 <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -89,7 +89,7 @@ const people = [
                     Go back to dashboard
                 </button>
                 </div>
-            </div>
+
         </>
 
        
