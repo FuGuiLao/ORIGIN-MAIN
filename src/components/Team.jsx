c73799eb82224585fc2802d3dd251900714f964f
Yi Min <eagleminy@gmail.com>
2023-04-20 20:43:26 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -196,7 +196,7 @@ export function Team() {
                                                                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                                                                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                                            >
-                                                           <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-14 sm:w-full sm:max-w-lg sm:p-14">
+                                                           <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all">
 
                                                                    
                                                                        
