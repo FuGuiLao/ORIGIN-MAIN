5fd991302af868428b43175260c50b1a4a3a90d1
Yi Min <eagleminy@gmail.com>
2023-04-27 17:59:13 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -131,8 +131,8 @@ export function Team() {
                                                                        leaveFrom="translate-x-0"
                                                                        leaveTo="translate-x-full"
                                                                    >
-                                                                       <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
-                                                                           <div className="flex h-2/3 align-bottom flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                                                                       <Dialog.Panel className="pointer-events-auto w-screen align-bottom max-w-2xl">
+                                                                           <div className="flex h-2/3  flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                                                                                <div className="px-4 sm:px-6">
                                                                                    <div className="flex items-start justify-between">
                                                                                        <Dialog.Title className="text-base font-semibold leading-6 text-gray-900">
