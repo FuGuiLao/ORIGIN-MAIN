fa0979a1ca71679b8f75fdf50a04cca7ab1e6ea8
Yi Min <eagleminy@gmail.com>
2023-04-27 00:48:49 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -29,10 +29,7 @@ const people = [
 
     modal:
  
-        <> <div className="px-4 py-10 sm:px-10 sm:py-16 md:py-20 lg:px-20 lg:py-32">
-            <SectionHeading number="5" id="author-title">
-                Our People
-            </SectionHeading>
+        <>
             <p className="mt-8 font-display text-5xl font-extrabold tracking-tight text-zinc-900 sm:text-6xl">
                 <span className="block text-red-800">Jayden Brant -</span> <bold>Hi, I am the founder and managing partner of Origin Investigations Inc.</bold>
             </p>
@@ -58,16 +55,6 @@ const people = [
                 Jayden is a member of the California Association of Licensed Investigators, the United States Association of Professional Investigators, the Association of Former Intelligence Officers, and the International Association of Law Enforcement Intelligence Analysts.
             </p>
 
-            <p className="mt-8">
-                <Link
-                    href="#"
-                    className="inline-flex items-center text-base font-medium tracking-tight text-red-800"
-                >
-
-                    <span className="ml-0"><a href="https://www.linkedin.com/in/jaydenbrant" target="_blank">View on LinkedIn</a></span>
-                </Link>
-            </p>
-        </div>
 
         </>
 
