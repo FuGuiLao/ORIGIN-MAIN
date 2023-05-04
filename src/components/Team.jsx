c0de82d43a24ca64cff4d54b20d739c14442153d
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-05-04 15:59:59 -0700

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -133,12 +133,7 @@ export function Team() {
                                                                 <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
                                                                   <Transition.Child
                                                               as={Fragment}
-         enter="transition-opacity duration-75"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="transition-opacity duration-150"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
+  
                                                               
                                                                   >
                                                                        <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
