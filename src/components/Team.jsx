b39da6a76cc6f37c5d8f7c8b7b0a31a9bf52ea52
Yi Min <eagleminy@gmail.com>
2023-05-04 14:29:56 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -133,17 +133,17 @@ export function Team() {
                                                        <div className="fixed inset-0" />
 
                                                        <div className="fixed inset-0 overflow-hidden">
-                                                           <div className="absolute inset-0  overflow-hidden">
-                                                               <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
-                                                                   <Transition.Child
-                                                                       as={Fragment}
-                                                                       enter="transform transition ease-in-out duration-1000 sm:duration-700"
-                                                                       enterFrom="translate-x-full"
-                                                                       enterTo="translate-x-0"
-                                                                       leave="transform transition ease-in-out duration-1000 sm:duration-700"
-                                                                       leaveFrom="translate-x-0"
-                                                                       leaveTo="translate-x-full"
-                                                                   >
+                                                              <div className="absolute inset-0 overflow-hidden">
+                                                                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
+                                                                  <Transition.Child
+                                                                    as={Fragment}
+                                                                    enter="transform transition ease-in-out duration-500 sm:duration-700"
+                                                                    enterFrom="translate-x-full"
+                                                                    enterTo="translate-x-0"
+                                                                    leave="transform transition ease-in-out duration-500 sm:duration-700"
+                                                                    leaveFrom="translate-x-0"
+                                                                    leaveTo="translate-x-full"
+                                                                  >
                                                                        <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
                                                                            <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                                                                                <div className="px-4 sm:px-6">
