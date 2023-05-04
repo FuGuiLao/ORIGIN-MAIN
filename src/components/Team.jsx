b10febb027b1e8919e30c50febf6a4c8399b0b6a
Yi Min <eagleminy@gmail.com>
2023-05-04 15:36:43 -0400

    1

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -137,7 +137,7 @@ export function Team() {
                                                                 <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
                                                                   <Transition.Child
                                                                     as={Fragment}
-                                                                    enter="transform-transition ease-in-out duration-500 sm:duration-700"
+                                                                    enter="transform transition ease-in-out duration-500 sm:duration-700"
                                                                     enterFrom="translate-x-full"
                                                                     enterTo="translate-x-0"
                                                                     leave="transform transition ease-in-out duration-500 sm:duration-700"
