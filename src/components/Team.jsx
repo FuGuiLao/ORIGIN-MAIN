cbc85a8dd7d67125a42ebb04f161a7bbf50e6848
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-05-04 16:05:38 -0700

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -119,30 +119,26 @@ export function Team() {
 
                                        
                                        <div>
-                                           <button onClick={() => setOpen(true)}>Open Slideover</button>
+                                           <button onClick={() => setOpen(true)}>Open Modal</button>
 
                                            {open && (
 
 
-                                               <Transition.Root show={open} as={Fragment}
-
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
->
+                                               <Transition.Root show={open}>
                                                    <Dialog as="div" className="relative z-10" onClose={setOpen}>
                                                        <div className="fixed inset-0" />
 
                                                        <div className="fixed inset-0 overflow-hidden">
                                                               <div className="absolute inset-0 overflow-hidden">
                                                                 <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
-                                                                  <Transition.Child>
-                                                       as={Fragment}
-  
-                                                              
+                                                                  <Transition.Child
+                                                                    as={Fragment}
+                                                                    enter="transform transition ease-in-out duration-1000 sm:duration-1000"
+                                                                    enterFrom="translate-x-full"
+                                                                    enterTo="translate-x-0"
+                                                                    leave="transform transition ease-in-out duration-1000 sm:duration-1000"
+                                                                    leaveFrom="translate-x-0"
+                                                                    leaveTo="translate-x-full"
                                                                   >
                                                                        <Dialog.Panel className="pointer-events-auto w-screen max-w-2xl">
                                                                            <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 border border-zinc-50 shadow-sm">
