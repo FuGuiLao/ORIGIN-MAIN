23fbc4500aecfc359ba745a08dded42352ca0fbb
IronHeart <ironheart0225@gmail.com>
2023-05-06 02:21:12 +0300

    remove .next intermediate directory which is unnecessary and too frequently changed for each developer

--- .next/static/lGOuQDT9lOzM9DNw_PjmZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/459-1b770b96b165bada.js","static/chunks/pages/index-f7cfcd56cedcf13a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
