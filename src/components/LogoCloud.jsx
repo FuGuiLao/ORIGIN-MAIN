a53b9555145f9d7ccdd75e18b5f95cf10a8563e6
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-08-17 17:07:55 -0400

    Update LogoCloud.jsx

--- src/components/LogoCloud.jsx
+++ src/components/LogoCloud.jsx
@@ -17,14 +17,14 @@ export function LogoCloud() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src={seal_federalcourt}
+            src={seal_bh}
             alt=""
             width={158}
             height={48}
           />
           <Image
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src={seal_casc}
+            src={seal_federalcourt}
             alt=""
             width={158}
             height={48}
