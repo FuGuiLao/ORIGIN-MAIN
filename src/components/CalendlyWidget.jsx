d228c5d61b1ba8a934ef696c50f0184d50dce1aa
alpinistdev <tony715225@gmail.com>
2024-04-23 00:56:57 -0700

    change gray background to white in MS booking

--- src/components/CalendlyWidget.jsx
+++ src/components/CalendlyWidget.jsx
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react';
+import { useEffect, useRef } from "react";
+
 
 function CalendlyWidget() {
-  useEffect(() => {
-    const head = document.querySelector('head');
-    const script = document.createElement('script');
-    script.setAttribute('src', 'https://assets.calendly.com/assets/external/widget.js');
-    head.appendChild(script);
-  }, []);
+  const iframeRef = useRef(null);
+
+  
+  // useEffect(() => {
+  //   // Function to handle script load event
+  //   // Create script element
+  //   const script = document.createElement('script');
+  //   script.setAttribute('src', 'https://assets.calendly.com/assets/external/widget.js');
+  //   script.addEventListener('load', handleScriptLoad);
+
+  //   // Append script to head
+  //   document.head.appendChild(script);
+  // }, []);
 
   return (
-    <div className="calendly-inline-widget" data-url="https://outlook.office365.com/book/ORIGINClientConsultation@origininvestigations.com/" style={{ minWidth: '320px', height: '100%' }} />
+    <>
+    <iframe ref={iframeRef}
+    src="https://outlook.office365.com/book/ORIGINClientConsultation@origininvestigations.com/"
+    style={{ minWidth: '350px', height: '100%' }} loading="lazy" />
+    <div className="fixed bg-white left-0 top-[50px] w-full h-[28px]" />
+    <div className="fixed bg-white left-0 top-[50px] w-[120px] h-full max-sm:hidden" />
+    <div className="fixed bg-white right-4 top-[50px] w-[120px] h-full max-sm:hidden" />
+    </>
+    // <div id="calendly-inline-widget" className="calendly-inline-widget" data-url="https://outlook.office365.com/book/ORIGINClientConsultation@origininvestigations.com/" style={{ minWidth: '350px', height: '100%' }} />
   );
 }
 
