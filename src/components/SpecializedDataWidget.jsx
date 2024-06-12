46e728da2cd5789ccbe8f47e78bc4f0c1d04f717
alpinistdev <tony715225@gmail.com>
2024-06-12 12:15:00 +1000

    [update] click 'Order Specialized Data Now' to a new window and new url

---
+++ src/components/SpecializedDataWidget.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+
+function SpecializedDataWidget() {
+  // useEffect(() => {
+  //   const head = document.querySelector('head')
+  //   const script = document.createElement('script')
+  //   script.setAttribute(
+  //     'src',
+  //     'https://assets.calendly.com/assets/external/widget.js'
+  //   )
+  //   head.appendChild(script)
+  // }, [])
+  // useEffect(() => {
+  //   window.open(
+  //     'https://get-in-touch-733c19.zapier.app/specialized-data-request',
+  //     '_blank'
+  //   )
+  // }, [])
+
+  return (
+    // <div
+    //   className="calendly-inline-widget"
+    //   data-url="https://get-in-touch-733c19.zapier.app/specialized-data-request"
+    //   style={{ minWidth: '320px', height: '100%' }}
+    // />
+    <iframe
+      src="https://get-in-touch-733c19.zapier.app/specialized-data-request"
+      width="100%"
+      height="100%"
+    />
+  )
+}
+
+export default SpecializedDataWidget
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Head from 'next/head'
 
-import { Banner } from '@/components/Banner' 
+import { Banner } from '@/components/Banner'
 import { OurPeople } from '@/components/OurPeople'
 import { Footer } from '@/components/Footer'
 import { FreeChapters } from '@/components/FreeChapters'
@@ -19,91 +19,98 @@ import { Team } from '@/components/Team'
 import { Button } from '@/components/Button'
 import CalendlySlideover from '@/components/CalendlySlideover'
 import HeyflowSlideover from '@/components/HeyflowSlideover'
+import SpecializedDataSlideover from '@/components/SpecializedDataSlideover'
 
 export default function Home() {
-  const [openSchedule, setOpenSchedule] = useState(false);
-  const [openHeyflow, setOpenHeyflow] = useState(false);
+  const [openSchedule, setOpenSchedule] = useState(false)
+  const [openHeyflow, setOpenHeyflow] = useState(false)
+  const [openSpecializedData, setOpenSpecializedData] = useState(false)
 
   return (
     <>
       <Head>
-        <title>
-          ORIGIN / Investigation / Intelligence
-        </title>
+        <title>ORIGIN / Investigation / Intelligence</title>
         <meta
           name="description"
           content="Origin is a private investigation and intelligence agency. We assist governments, corporations, law firms, and individuals."
         />
         <meta name="apple-mobile-web-app-title" content="ORIGIN" />
-          <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(a,b,c,d,e,f,g){c[d]=c[d]||function(){(c[d].q=c[d].q||[]).push(arguments)};c['_lsAlias']=c[d];e=a.createElement(b);e.type='text/javascript';e.async=true;e.src='https://app.chatsupport.co/api/client/get/script/LS-42bf8cb7';f=function(){g=a.getElementsByTagName(b)[0];g.parentNode.insertBefore(e,g)};c.addEventListener('load',f)})(document,'script',window,'_ls');_ls('init',{'projectId':'LS-42bf8cb7'});
-            `
+            `,
           }}
         />
         <script src="https://static.heyflow.app/widget/latest/webview.js"></script>
       </Head>
-    <Banner />
-    <Hero />
+      <Banner />
+      <Hero />
       <Introduction />
       <NavBar />
       <OurServices />
-      <Testimonial
-        id="testimonial-from-tommy-stroman"
-        author={{
-          
-        }}
-      >
-      <title>
-          CA SUPERIOR COURT CASES
-        </title>
-        <p className={`${openSchedule ? "pr-[17px] sm:pr-0" : ""}`}>
-          Origin is a member of the <b>United States Federal Court and the California Superior Court Investigator Panel</b>. This means that we can provide comprehensive criminal case investigative services at no cost to qualified defendants.
+      <Testimonial id="testimonial-from-tommy-stroman" author={{}}>
+        <title>CA SUPERIOR COURT CASES</title>
+        <p className={`${openSchedule ? 'pr-[17px] sm:pr-0' : ''}`}>
+          Origin is a member of the{' '}
+          <b>
+            United States Federal Court and the California Superior Court
+            Investigator Panel
+          </b>
+          . This means that we can provide comprehensive criminal case
+          investigative services at no cost to qualified defendants.
         </p>
+        <p>&nbsp;</p>
         <p>
-        &nbsp;
+          We also offer these services in partnership with law firms through our{' '}
+          <b>fully integrated case review</b> process. Our highly experienced
+          investigators can help make the difference in court.
         </p>
-       <p>
-         We also offer these services in partnership with law firms through our <b>fully integrated case review</b> process. Our highly experienced investigators can help make the difference in court.
-        </p>
-       <p>
-        &nbsp;
-        </p>
+        <p>&nbsp;</p>
         <Button color="red" onClick={() => setOpenSchedule(true)}>
           Schedule a Consultation
         </Button>
-        <CalendlySlideover open={openSchedule} onClose={() => setOpenSchedule(false)} />
+        <CalendlySlideover
+          open={openSchedule}
+          onClose={() => setOpenSchedule(false)}
+        />
       </Testimonial>
- 
-      <Investigation />
-      <Testimonial
-        id="testimonial-from-gerardo-stark"
-         author={{
-          
-        }}
-      >
-       <title>
-          SPECIALIZED DATA
-        </title>
-        <p className={`${openHeyflow ? "pr-[17px] sm:pr-0" : ""}`}>
-          Origin's specialized data services can <b>uncover hidden information</b> that can be valuable in criminal and civil cases. Our intelligence analysts can bring relevance to complex information. 
 
-            </p>
-        <p>
-        &nbsp;
+      <Investigation />
+      <Testimonial id="testimonial-from-gerardo-stark" author={{}}>
+        <title>SPECIALIZED DATA</title>
+        <p className={`${openHeyflow ? 'pr-[17px] sm:pr-0' : ''}`}>
+          Origin's specialized data services can{' '}
+          <b>uncover hidden information</b> that can be valuable in criminal and
+          civil cases. Our intelligence analysts can bring relevance to complex
+          information.
         </p>
- <p className={`${openHeyflow ? "pr-[17px] sm:pr-0" : ""}`}>
-          We specialize in <b>impossible to get information</b>, including bank and asset locates, insurance policy information, employment data, medical records, travel information, and social media activity.
-           </p> 
- <p>
-        &nbsp;
+        <p>&nbsp;</p>
+        <p className={`${openHeyflow ? 'pr-[17px] sm:pr-0' : ''}`}>
+          We specialize in <b>impossible to get information</b>, including bank
+          and asset locates, insurance policy information, employment data,
+          medical records, travel information, and social media activity.
         </p>
-        <Button color="red" onClick={() => setOpenHeyflow(true)}>
+        <p>&nbsp;</p>
+        <Button
+          color="red"
+          onClick={() => {
+            window.open(
+              'https://get-in-touch-733c19.zapier.app/specialized-data-request',
+              '_blank'
+            )
+          }}
+        >
           Order Specialized Data Now
         </Button>
-        <HeyflowSlideover open={openHeyflow} onClose={() => setOpenHeyflow(false)} title="Order Specialized Data Now" flowId="specialized-data-request" />
-      
+        {/* <SpecializedDataSlideover
+          open={openSpecializedData}
+          onClose={() => setOpenSpecializedData(false)}
+        /> */}
+        {/* <Button color="red" onClick={() => setOpenHeyflow(true)}>
+          Order Specialized Data Now
+        </Button>
+        <HeyflowSlideover open={openHeyflow} onClose={() => setOpenHeyflow(false)} title="Order Specialized Data Now" flowId="specialized-data-request" /> */}
       </Testimonial>
       <Resources />
       <FreeChapters />
@@ -111,12 +118,8 @@ export default function Home() {
       <Testimonials />
       <OurPeople />
       <Team />
-      <Contact /> 
+      <Contact />
       <Footer />
-      </>
-
-
+    </>
   )
-
-  
 }
