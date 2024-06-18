e1f8844b2faeaaba36ccc751c58cc062e1c61512
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-06-18 02:14:08 -0700

    Update JobSlideover.jsx

--- src/components/JobSlideover.jsx
+++ src/components/JobSlideover.jsx
@@ -62,8 +62,8 @@ const JobSlideover = ({ open, onClose }) => {
 	 <p className="mt-0 mb-0 text-sm tracking-tight text-zinc-700 px-10 py-1"><b>• Retirement Plans:</b> 401(k) plans to help you plan for your future.</p>
 	 <p className="mt-0 mb-0 text-sm tracking-tight text-zinc-700 px-10 py-1"><b>• Life Insurance:</b> Company-provided life insurance to ensure peace of mind for you and your loved ones.</p>
 	 <p className="mt-0 mb-0 text-sm tracking-tight text-zinc-700 px-10 py-1"><b>• Long-Term Disability Insurance:</b> Financial protection in case of prolonged illness or injury, ensuring you have support during challenging times.</p>
- <p className="mt-0 mb-0 text-sm tracking-tight text-zinc-700 px-10 py-5"><b>Human Resources Support:</b></p>
- <p className="mt-0 mb-0 text-sm tracking-tight text-zinc-700 px-10 py-5">Our dedicated human resources team is here to support you throughout your career journey. From onboarding to ongoing development, we ensure you have the resources and support needed to thrive at Origin. We foster a positive and inclusive workplace culture where every employee feels valued and empowered.</p>
+ <p className="mt-0 mb-0 text-sm tracking-tight text-zinc-700 px-10 pt-4 pb-1"><b>Human Resources Support:</b></p>
+ <p className="mt-0 mb-0 text-sm tracking-tight text-zinc-700 px-10 py-1">Our dedicated human resources team is here to support you throughout your career journey. From onboarding to ongoing development, we ensure you have the resources and support needed to thrive at Origin. We foster a positive and inclusive workplace culture where every employee feels valued and empowered.</p>
 <p className="mt-0 mb-10 text-sm tracking-tight text-zinc-700 px-10 py-5">Join us at Origin and be a part of a dynamic team that values excellence, innovation, and collaboration. Please click <a href="https://jobs.gusto.com/boards/origin-careers-e5e7c0e6-d25a-4784-9966-aad2669ee730" target="_blank"><b>here</b></a> to get started.</p>
    
   
