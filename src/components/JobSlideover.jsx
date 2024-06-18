d39f576a06f7d6a64f16820db2f0e0e7176d226b
FuGuiLao <81949764+FuGuiLao@users.noreply.github.com>
2024-06-18 01:15:03 -0700

    Update JobSlideover.jsx

--- src/components/JobSlideover.jsx
+++ src/components/JobSlideover.jsx
@@ -44,8 +44,8 @@ const JobSlideover = ({ open, onClose }) => {
                     </div>
       
         <p className="mt-4 text-lg tracking-tight text-zinc-700 px-10 py-5">
-         Origin partners with Gusto from the inital application to after you are hired to provide the highest quality human resources experience.</p>
-          <p><b>Hiring Process</b><br></br>
+         Origin partners with Gusto from the inital application to after you are hired to provide the highest quality human resources experience.<br></br><br></br>
+          <b>Hiring Process</b><br></br>
           1) Initial Online Application<br></br>
           2) VideoAsk Pre-Interview<br></br>
           3) Microsoft Teams Interview<br></br>
@@ -53,8 +53,8 @@ const JobSlideover = ({ open, onClose }) => {
           5) Offer Letter<br></br>
           6) Background Investigation<br></br>
           7) Hired / Onboarding Process
-            </p>
-          <p>Please click <a href="https://jobs.gusto.com/boards/origin-careers-e5e7c0e6-d25a-4784-9966-aad2669ee730" target="_blank"><b>here</b></a> to view available positions and begin the process.
+       <br></br><br></br> 
+        Please click <a href="https://jobs.gusto.com/boards/origin-careers-e5e7c0e6-d25a-4784-9966-aad2669ee730" target="_blank"><b>here</b></a> to view available positions and begin the process.
         </p>
    
   
